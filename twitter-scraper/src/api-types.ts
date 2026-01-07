bd03f37a39f4e3f41efef3432a6f0cc9b623c45f
jack <jack@pop-os.bandicoot-nominal.ts.net>
2026-01-07 17:56:52 +0000

    Refactor: Switch to NPM version of twitter-scraper and remove local clone

--- twitter-scraper/src/api-types.ts
+++
@@ -1,3 +0,0 @@
-// For some reason using Parameters<typeof fetch> reduces the request transform function to
-// `(url: string) => string` in tests.
-export type FetchParameters = [input: RequestInfo | URL, init?: RequestInit];
